62fe2014cfc0d82aac986b999a1a30e47937fd82
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-05-23 19:28:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,23 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> newTree;
+  std::ifstream file(filename);
+  std::string word = "";
+  if (file.is_open()) {
+    while (!file.eof()) {
+      word = "";
+      while (true) {
+        char letter = file.get();
+        if ((letter >= 97 && letter <= 122) || (letter >= 65 && letter <= 90)) {
+          char s = tolower(letter);
+          word = word + s;
+        } else {
+          break;
+        }
+      }
+      newTree.add(word);
+    }
+    return newTree;
+  }
 }
