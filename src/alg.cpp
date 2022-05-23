319fe5c3e250e21d7202bec4dfc2a9a40ed736dc
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-05-23 19:57:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,8 +15,7 @@ BST<std::string> makeTree(const char* filename) {
       while (true) {
         char letter = file.get();
         if ((letter >= 97 && letter <= 122) || (letter >= 65 && letter <= 90)) {
-          letter = tolower(letter);
-          word += letter;
+          word += tolower(letter);
         } else {
           break;
         }
